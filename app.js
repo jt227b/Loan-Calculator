b8541bc4d74679205332451917036fc11e091b96
Jose Torres <jt227b@gmail.com>
2019-09-06 22:34:33 -0500

    Added an Error Message to missing numbers

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ document
   .getElementById("loan-form")
   .addEventListener("submit", calculateResults);
 
+
 // Calculate Results Function
 function calculateResults(e) {
   console.log("Calculating...");
+
+
   // UI Variables
   const amount = document.getElementById("amount");
   const interest = document.getElementById("interest");
@@ -28,8 +31,36 @@ function calculateResults(e) {
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
   } else {
-    console.log("Please Check your numbers");
+    // Error Message Here
+    showError('Please check your numbers');
   }
 
   e.preventDefault();
+}
+
+// Show Error
+function showError(error) {
+  // Create a div
+  const errorDiv = document.createElement('div');
+
+  // Get Elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Add class
+  errorDiv.className = 'alert alert-danger';
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000);
+}
+
+// Clear Error
+function clearError() {
+  document.querySelector('.alert').remove();
 }
